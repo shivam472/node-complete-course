dc50d958f7d077524614c5f8e579acd36519f231
shivam472 <shivamraj472@gmail.com>
2021-10-18 16:41:37 +0530

    parsing incoming requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', (req, res, next) => {
     console.log('This always runs');
@@ -9,8 +12,13 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
     console.log('In the middleware');
-    res.send('<h1>The add product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">submit</button></form>');
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
     console.log('In another middleware');
