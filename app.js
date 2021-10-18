88760ed383f098b44ea508b5398f5a5ad4329fa9
shivam472 <shivamraj472@gmail.com>
2021-10-18 15:54:30 +0530

    using the node modules system

--- app.js
+++ app.js
@@ -1,49 +1,10 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const routes = require('./routes');
 
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My first page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></body>');
-        res.write('</html>');
-        return res.end();
-    }
+console.log(routes.someText);
 
-    // Redirecting requests
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-
-            // Blocking & Non-Blocking code
-            // fs.writeFileSync('message.txt', message);
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my Node.js server</h1></body>');
-    res.write('</html>');
-    res.end();
-
-});
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
 
