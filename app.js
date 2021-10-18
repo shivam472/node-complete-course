afd7f4d66c18c2cfc5c95f0a8b52c8efc0077896
shivam472 <shivamraj472@gmail.com>
2021-10-18 16:31:57 +0530

    handling different routes (correct)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('This always runs');
     next();
 });
