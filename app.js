58ce598e060104bf41b5736032b2ae9d787adebc
shivam472 <shivamraj472@gmail.com>
2021-10-18 19:57:32 +0530

    adding a 404 error page

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.listen(4000);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 
+app.listen(4000);
