5cb7ece7a73faf334bdd02de1937d23f07a07f72
shivam472 <shivamraj472@gmail.com>
2021-10-18 17:06:18 +0530

    using express router

--- app.js
+++ app.js
@@ -3,29 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">submit</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>');
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(4000);
-
-
 
